74698db27651b2962f32709ba21bd447ed2713d0
Souldagwan149 <Souldagwan149@gmail.com>
2025-03-02 17:28:49 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -20,7 +20,7 @@ CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
 STICKER_NAME: process.env.STICKER_NAME || "✦MALVIN-XD✦",
 // type sticker pack name  
 
-CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️‍🔥,❤️‍🩹,❤️,🩷,🧡,💛,💚,💙,🩵,💜,🤎,🖤,🩶,🤍",
+CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "",
 // chose custom react emojis by yourself 
 
 DELETE_LINKS: process.env.DELETE_LINKS || "false",
